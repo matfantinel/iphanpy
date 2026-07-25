91bba3b51e065dfbdc1e33643d31f23d2f6d779b
Matt Fantinel <matt@fantinel.dev>
2026-07-25 22:37:12 +0200

    Add iPhanpy's own User Agent; icon update

--- ios/App/App/MyViewController.swift
+++ ios/App/App/MyViewController.swift
@@ -1,7 +1,15 @@
 import UIKit
+import WebKit
 import Capacitor
 
 class MyViewController: CAPBridgeViewController {
+    override func webViewConfiguration(for instanceConfiguration: InstanceConfiguration) -> WKWebViewConfiguration {
+        let configuration = super.webViewConfiguration(for: instanceConfiguration)
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "unknown"
+        configuration.applicationNameForUserAgent = "iPhanpy/\(version) (+https://github.com/matfantinel/iphanpy)"
+        return configuration
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         webView!.allowsBackForwardNavigationGestures = true
